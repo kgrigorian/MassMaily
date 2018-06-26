79a611da1757b4b6e92e7a61b2cf6895a9a2ac6f
karenche <karen.grigorjan@gmail.com>
2018-06-27 01:28:57 +0300

    minor 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({hi: 'there aahahahahahahgain!' })
+    res.send({hi: 'there hahahgain!' })
 } );
 
 const PORT = process.env.PORT || 5000;
