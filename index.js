bf978cf5226c183081f7cd2100d22226b23d3852
karenche <karen.grigorjan@gmail.com>
2018-06-27 01:18:06 +0300

    second trial

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({hi: 'there' })
+    res.send({hi: 'there again!' })
 } );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
-});
