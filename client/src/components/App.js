962c5583d89d972582c313789a4520a6c54f81c2
Karen Grigorjan <kar3n@inbox.ru>
2018-07-03 20:10:17 +0300

    client header and landing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect} from 'react-redux';
+import * as actions from '../actions';
 
+import Header from './Header';
+import Landing from './Landing';
+const Dashboard = () => <h2></h2>
+const SurveyNew = () => <h2></h2>
 
-const App = () => {
-    return(
-        <div>
-            Hi bits
-        </div>
-    );
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    render() {
+        return(
+            <div className="container">
+                <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+                </BrowserRouter>
+            </div>
+        );
+}
 }
 
-export default App;
+export default connect(null,actions)(App);
